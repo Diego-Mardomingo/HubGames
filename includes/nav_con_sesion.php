4155960ed83345a8a62c1dfaf9b58864d4f3290b
Diego López Mardomingo <diego.lopez.mardomingo@gmail.com>
2023-04-23 16:54:35 +0200

    v1.4 - Corrección bugs autenticación

--- includes/nav_con_sesion.php
+++ includes/nav_con_sesion.php
@@ -1,5 +1,6 @@
 <div class="nav">
   <a href="https://HubGames.es"><img src="https://HubGames.es/img/HubGamesLogo.webp" title="HubGames" alt="Logo HubGames"></a>
+  <div class="barras">Menú <i class="fa-solid fa-bars"></i></div>
   <div class="nav_list">
     <div class="nav_item"><a href="https://HubGames.es">Buscador</a></div>
     <div class="nav_item"><a href="">JUDI</a></div>
--- includes/nav_sin_sesion.php
+++ includes/nav_sin_sesion.php
@@ -1,11 +1,11 @@
 <div class="nav">
-  <a href="http://localhost/HubGames"><img src="http://localhost/HubGames/img/HubGamesLogo.webp" title="HubGames" alt="Logo HubGames"></a>
+  <a href="https://HubGames.es"><img src="https://HubGames.es/img/HubGamesLogo.webp" title="HubGames" alt="Logo HubGames"></a>
   <div class="barras">Menú <i class="fa-solid fa-bars"></i></div>
   <div class="nav_list">
-    <div class="nav_item"><a href="http://localhost/HubGames">Buscador</a></div>
+    <div class="nav_item"><a href="https://HubGames.es">Buscador</a></div>
     <div class="nav_item"><a href="">JUDI</a></div>
     <div class="nav_item"><a href="">Chats</a></div>
-    <div class="nav_item"><a href="http://localhost/HubGames/vistas/login_vista.php">Iniciar sesión</a></div>
-    <div class="nav_item"><a href="http://localhost/HubGames/vistas/registro_vista.php">Registrarse</a></div>
+    <div class="nav_item"><a href="https://HubGames.es/vistas/login_vista.php">Iniciar sesión</a></div>
+    <div class="nav_item"><a href="https://HubGames.es/vistas/registro_vista.php">Registrarse</a></div>
   </div>
 </div>
